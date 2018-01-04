3e12463f4f3276fb8c859e98115e43cf56214651
Андрей Тихоновский <andrew@sargas.at.home>
2018-01-05 00:32:09 +0200

    added some comments and corrected some other

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /** \file main.cpp
- * This is my first Qt project. The source is slightly modified "Hello World"
+ * This is my first Qt project. The source is modified "Hello World"
  * from some lessons on youtube.
  *
  * Author: Andrei Tihonovschi
@@ -10,7 +10,7 @@
 #include <QLabel>
 
 /**
- * @brief DEFAULT_TEXT This is default text to be displayed in the main widget window.
+ * @brief DEFAULT_TEXT This is default greeting to be displayed in the main widget window.
  */
 const char *DEFAULT_TEXT = "<span style=\"color: red\">Hello world!</span>";
 
@@ -22,26 +22,35 @@ const char *DEFAULT_TEXT = "<span style=\"color: red\">Hello world!</span>";
 class MyQApp:public QApplication
 {
 private:
-    QLabel *lbl;
+    QLabel *lbl; //!< QLabel which will display the greetings text
 
 public:
     MyQApp(int argc, char **argv): QApplication(argc, argv), lbl(NULL)
     {
+        // if there are no command line parameters, use default greeting
         const char *txt = DEFAULT_TEXT;
-        if(argc > 1)
+        if(argc > 1) // or, if there are, use first CLI argument as greeting
             txt = argv[1];
 
+        // create and show QLabel window
         lbl = new QLabel(txt);
         lbl->show();
     }
 
     ~MyQApp()
     {
+        // delete QLabel, if it was created
         if(lbl)
             delete lbl;
     }
 };
 
+/**
+ * @brief main application entry point
+ * @param argc CLI arguments counter
+ * @param argv CLI arguments
+ * @return 0 if no error, other value if an error has occured
+ */
 int main(int argc, char **argv)
 {
     MyQApp a(argc, argv);
